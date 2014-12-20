b24dd5fe441b2cae0f28b8a3dc347361d48db7bd
wuchangqian <30889515@qq.com>
2014-12-21 00:21:21 +0800

    modify ruby/l.rb

--- ruby/l.rb
+++ ruby/l.rb
@@ -4,6 +4,6 @@ hash = { "key1" => "val1", "key2" => "val2" }
 #puts string
 string = ""
 hash.each { |k,v| string << k << " is " << v << "\n" }
-#puts string;
+puts string;
 
-3.upto(300)do |i| puts i ; end;
+3.upto(8)do |i| puts i ; end;
